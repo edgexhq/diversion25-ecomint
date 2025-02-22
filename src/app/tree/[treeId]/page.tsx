d9f3af70b130f8d1e1161e120a40efe2cd2de628
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2025-02-22 23:45:14 +0530

    feat(tree): add TreeCertificate component to display tree details

---
+++ src/app/tree/[treeId]/page.tsx
@@ -0,0 +1,26 @@
+import { getTree } from "@/actions/form";
+import Treecertificate from "@/components/Treecertificate";
+
+export default async function TreeCertificate({
+  params,
+}: {
+  params: Promise<{ treeId: string }>;
+}) {
+  const { treeId } = await params;
+
+  const treedetails = await getTree(treeId);
+  if (!treedetails?.data) {
+    return (
+      <div className="min-h-screen w-full text-center flex items-center justify-center">
+        <p className="text-5xl font-bold tracking-tight">Tree not found</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="min-h-screen bg-white p-4 md:p-8">
+      <Treecertificate details={treedetails.data} />
+    </div>
+
+  );
+}
